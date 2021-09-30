409ffa2b4dc570700f3bcee57e00d7f7b9a3de14
Karlo Verde <karlo.verde.1996@gmail.com>
2021-09-29 23:47:01 -0500

    feat: set up navigation

--- lib/app.dart
+++ lib/app.dart
@@ -1,28 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:sentry_example/screens/a_screen.dart';
+import 'package:sentry_example/screens/b_screen.dart';
+import 'package:sentry_example/screens/c_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => const MaterialApp(
+  Widget build(BuildContext context) => MaterialApp(
         title: 'Sentry Example',
-        home: HomeScreen(),
-      );
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Home Screen'),
-        ),
-        body: Center(
-          child: Text(
-            'Home Screen',
-            style: Theme.of(context).textTheme.headline1,
-          ),
-        ),
+        initialRoute: AScreen.routeName,
+        routes: {
+          AScreen.routeName: (context) => const AScreen(),
+          BScreen.routeName: (context) => const BScreen(),
+          CScreen.routeName: (context) => const CScreen(),
+        },
       );
 }
